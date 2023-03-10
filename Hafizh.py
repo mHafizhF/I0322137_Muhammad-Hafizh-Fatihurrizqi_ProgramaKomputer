81e587093346ac5d6a2285357d9e7c8a1e5b269e
hafizh <fatiihoerrizq1@gmail.com>
2023-03-10 10:17:27 +0700

    hafizh

---
+++ Hafizh.py
@@ -0,0 +1,13 @@
+nama_depan = "Hafizh"
+nama_blk = "Fatihurrizqi"
+
+angka = 10
+desimal = 0.5
+boolean = True
+kelas_D =["Hafizh", "kls d", "dll"]
+data ={"nama": "Hafizh", "Kelas": " KLS D"}
+
+print(nama_depan)
+print("nama saya adalah hafizh")
+
+print(f"nama saya adalah {nama_depan} {nama_blk} dari kelas {data['Kelas']}")
