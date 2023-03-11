ed9ca3d647a0995c04999fe4f4038594549ea369
hafizh <fatiihoerrizq1@gmail.com>
2023-03-11 15:26:47 +0700

    Exercise_01

---
+++ Exercise_01.py
@@ -0,0 +1,16 @@
+#input
+nama = 'Muhammad Hafizh Fatihurrizqi'
+nim = 'I0322137'
+alamat = 'Karangasaem, Laweyan, Surakarta, Jawa Tengah'
+visualisasi = ' kota yang sarat akan kebudayaannya. Selain kebudayaannya yang kental dikalangan masyarakat,masyarakat kota solo juga menjunjung tinggi tata krama atau unggah-ungguh. '
+harapan = 'Menjadi lulusan yang memiliki integritas serta etika dalam pekerjaan.'
+
+dict = {'a' : nama, 'b' : nim, 'c' : alamat, 'd' : visualisasi, 'e' : harapan}
+
+
+#hasil
+print("Nama saya adalah", dict['a'])
+print("NIM saya adalah", dict['b'])
+print("Saya tinggal di", dict['c'])
+print("Kota solo merupakan ", dict['d'])
+print("Harapan saya setelah lulusan dari Teknik Industri adalah", dict['e'])
